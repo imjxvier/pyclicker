759897fc2869913f3c3991646f25a24c33d8ef35
imjxvier <imjaviermarin@gmail.com>
2018-08-08 09:48:35 -0400

    autoclicker fixes

--- autoclicker.py
+++ autoclicker.py
@@ -2,20 +2,20 @@ import keyboard, pyautogui
 
 on, off = 0, 1
 
-print('Welcome to pyclicker!')
+print('\nWelcome to pyclicker!')
 
 while True:
     try:
-        cps = int(input('Tell me how many CPS you wanna get. 1 = 10, 2 = 20, 3 = 30, 4 = 40 and like that: '))
+        cps = int(input('\nTell me how many CPS you wanna get. 1 = 10, 2 = 20, 3 = 30, 4 = 40 and like that: '))
     except ValueError:
-        print('Invalid input. Your input needs to be a number.\n')
+        print('Invalid input. Your input needs to be a number.')
     except KeyboardInterrupt:
-        print('\n')
+        print('\nGoodbye!')
+        break
     else:
-        print('Thanks!\n')
         break
 
-print('pyclicker\nF1 = ON\nF2 = OFF\nF = Start clicking (When ON)\nCTRL+C = Quit')
+print('\npyclicker\nF1 = ON\nF2 = OFF\nF = Start clicking (When ON)\nCTRL+C = Quit')
 
 try:
     while True:
@@ -29,4 +29,4 @@ try:
             print('Goodbye!')
             break
 except KeyboardInterrupt:
-    print('Goodbye!')
+    print('\nGoodbye!')
